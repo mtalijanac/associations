bd4d3001d9884c48bc4134a397a667e0ed474145
mtalijanac <marko.talijanac@gmail.com>
2025-07-07 09:55:46 +0200

    minor improvments

--- src/test/java/mt/fireworks/associations/CompactMapTest.java
+++ src/test/java/mt/fireworks/associations/CompactMapTest.java
@@ -9,10 +9,10 @@ import org.eclipse.collections.api.block.procedure.primitive.ObjectLongProcedure
 
 public class CompactMapTest {
 
-    
+
     public static void main(String[] args) {
         SerDes<String> serDes = Associations.stringSerDes(StandardCharsets.UTF_8);
-        
+
         Function<String, byte[]> keyer = new Function<String, byte[]>() {
             @Override
             public byte[] apply(String val) {
@@ -22,31 +22,31 @@ public class CompactMapTest {
                 return key;
             }
         };
-        
-        
-        CompactMap<String> map = new CompactMap<>(3, serDes, keyer);
-        
+
+
+        CompactMap<String> map = new CompactMap<>(serDes, keyer);
+
         String marko = "marko";
         map.add(marko);
         map.add(marko);
-        
+
         map.add("markovo");
-        
+
         map.add("12345678");
         map.add("123456789");
         map.add("12345678A");
         printEntries(map);
-        
+
         map.tick();
         printEntries(map);
-        
+
         map.forEach(new Procedure<String>() {
             public void value(String each) {
                 System.out.println(each);
             }
         });
     }
-    
+
     static void printEntries(CompactMap<String> map) {
         map.index.forEachKeyValue(new ObjectLongProcedure<byte[]>() {
             public void value(byte[] key, long parameter) {
@@ -55,7 +55,7 @@ public class CompactMapTest {
                 System.out.println(keyCh + " -> " + livePointer );
             }
         });
-        
+
         System.out.println();
     }
 }
